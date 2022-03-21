42dab1145ef9e66d6beb940147ac1afb804827bf
Boris Dryabin <b.dryabin@rt-solar.ru>
2022-03-21 23:21:17 +0300

    add map

--- app/js/map.js
+++ app/js/map.js
@@ -1,14 +1,14 @@
-mapboxgl.accessToken =
-  "pk.eyJ1IjoiYmFyczExODkiLCJhIjoiY2sydDk2bGxpMTViMjNpcWJieXR2YW03biJ9.gJoHbjoQT-xmMVDBHOI3Mg";
-const map = new mapboxgl.Map({
-  container: "map",
-  style: "mapbox://styles/mapbox/light-v10",
-  center: [30.385083,59.89424],
-  zoom: 19,
-  reverseGeocode: false
-});
+// mapboxgl.accessToken =
+//   "pk.eyJ1IjoiYmFyczExODkiLCJhIjoiY2sydDk2bGxpMTViMjNpcWJieXR2YW03biJ9.gJoHbjoQT-xmMVDBHOI3Mg";
+// const map = new mapboxgl.Map({
+//   container: "map",
+//   style: "mapbox://styles/mapbox/light-v10",
+//   center: [30.385083,],
+//   zoom: 19,
+//   reverseGeocode: false
+// });
 
-const marker2 = new mapboxgl.Marker()
-  .setLngLat([30.385083,59.89424])
-  .addTo(map);
-  
+// const marker2 = new mapboxgl.Marker()
+//   .setLngLat([30.385083,59.89424])
+//   .addTo(map);
+//   ,
